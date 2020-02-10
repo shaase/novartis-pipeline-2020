a29b9d4092924cb320f08f34d18d75a090552529
Stephen Haase <shaase@gmail.com>
2020-02-10 12:37:30 -0700

    study table working

--- electron/app/components/Pipeline/components/PipelineBubble/index.tsx
+++ electron/app/components/Pipeline/components/PipelineBubble/index.tsx
@@ -1,10 +1,10 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
 import { FilterContext, PipelineContext } from "../../state";
+import { postBubbleUpdate, subscribeToBubbleUpdates } from "../../workers";
 import PanelHeader from "../PanelHeader";
 import BubbleChart from "./bubble-chart";
-// import StudyTable from '../StudyTable';
+import StudyTable from "../StudyTable";
 import CardViewer from "../CardViewer";
-import { postBubbleUpdate, subscribeToBubbleUpdates } from "../../workers";
 import { colorForData } from "../../data";
 import { itemsForPath } from "../../utils";
 import { PipelineItem, Bubble } from "../../types";
@@ -27,7 +27,7 @@ const defaultData: WorkerData = {
 };
 
 const PipelineBubble: React.FC = () => {
-  const { path, compound, onNavigate } = useContext(PipelineContext);
+  const { scale, path, compound, onNavigate } = useContext(PipelineContext);
   const { phases } = useContext(FilterContext);
   const [workerData, setWorkerData] = useState<WorkerData>(defaultData);
   const pathRef = useRef("");
@@ -50,7 +50,7 @@ const PipelineBubble: React.FC = () => {
   const showBubbles =
     (root === "Tumors" && level < 5 && data.length > 1) || (root === "Compounds" && level < 4 && data.length > 1);
   const isEmpty = data.length === 0 && studyCode === undefined;
-
+  console.log(showBubbles);
   return (
     <div className={isEmpty ? styles.empty : styles.container}>
       <PanelHeader category="Study" />
@@ -67,9 +67,9 @@ const PipelineBubble: React.FC = () => {
       />
 
       {/* TABLE */}
-      {/* {!showBubbles && studyCode === undefined && (
-        <StudyTable scale={scale} panel={panel} data={data} compound={compound} onSelect={onSelect} />
-      )} */}
+      {!showBubbles && studyCode === undefined && (
+        <StudyTable scale={scale} data={data} compound={compound} onNavigate={onNavigate} />
+      )}
 
       {/* STUDY */}
       {studyCode !== undefined && (
