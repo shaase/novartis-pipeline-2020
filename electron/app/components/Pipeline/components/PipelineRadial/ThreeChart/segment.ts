83e8f675734fc5d73012d3f0d7e2543eb662008d
Stephen Haase <shaase@gmail.com>
2020-02-28 15:42:18 -0700

    working on full circle

--- electron/app/components/Pipeline/components/PipelineRadial/ThreeChart/segment.ts
+++ electron/app/components/Pipeline/components/PipelineRadial/ThreeChart/segment.ts
@@ -1,13 +1,14 @@
-import { Mesh, MeshBasicMaterial, Shape, ShapeBufferGeometry } from "three";
+import * as THREE from "three";
 import { RadialNode, NodeArc } from "../../../types";
 import fixedNode from "./fixed-node";
+import ThreeRadial from ".";
 
 const hexColor = (hexString: string): number => {
   const str = `${hexString}`.replace(/[^\da-f]/gi, "");
   return parseInt(str, 16);
 };
 
-class Segment extends Mesh {
+class Segment extends THREE.Mesh {
   node: RadialNode;
 
   getArc: (node: RadialNode) => NodeArc;
@@ -20,13 +21,13 @@ class Segment extends Mesh {
     const x1 = Math.cos(endAngle) * innerRadius;
     const y1 = Math.sin(endAngle) * innerRadius;
 
-    const segment = new Shape();
+    const segment = new THREE.Shape();
     segment.absarc(0, 0, outerRadius, startAngle, endAngle, false);
     segment.lineTo(x1, y1);
     segment.absarc(0, 0, innerRadius, endAngle, startAngle, true);
 
-    const geometry = new ShapeBufferGeometry(segment, 64);
-    const material = new MeshBasicMaterial({ color: hexColor(n.color) });
+    const geometry = new THREE.ShapeBufferGeometry(segment, 64);
+    const material = new THREE.MeshBasicMaterial({ color: hexColor(n.color) });
     // material.transparent = true;
     // material.opacity = 0.4;
     super(geometry, material);
@@ -35,20 +36,33 @@ class Segment extends Mesh {
   }
 
   update(path: string): void {
-    if (this.geometry instanceof ShapeBufferGeometry) {
+    if (this.geometry instanceof THREE.ShapeBufferGeometry) {
       const fn = fixedNode(this.node, path);
       const { startAngle, endAngle, innerRadius, outerRadius } = this.getArc(fn);
       const x1 = Math.cos(endAngle) * innerRadius;
       const y1 = Math.sin(endAngle) * innerRadius;
 
-      const segment = new Shape();
-      segment.absarc(0, 0, outerRadius, startAngle, endAngle, false);
-      segment.lineTo(x1, y1);
-      segment.absarc(0, 0, innerRadius, endAngle, startAngle, true);
+      const max = Math.max(startAngle, endAngle);
+      const min = Math.min(startAngle, endAngle);
+      const diff = Math.round((max - min) * 100) / 100;
+      const isCircle = diff === 6.28;
 
-      const geometry = new ShapeBufferGeometry(segment, 64);
-      const { position } = geometry.attributes;
-      this.geometry.setAttribute("position", position);
+      if (isCircle) {
+        const geometry = new THREE.RingBufferGeometry(innerRadius, outerRadius, 128);
+        const { position } = geometry.attributes;
+        console.log(position.count);
+        this.geometry.setAttribute("position", position);
+      } else {
+        const segment = new THREE.Shape();
+        segment.absarc(0, 0, outerRadius, startAngle, endAngle, false);
+        segment.lineTo(x1, y1);
+        segment.absarc(0, 0, innerRadius, endAngle, startAngle, true);
+
+        const geometry = new THREE.ShapeBufferGeometry(segment, 64);
+        const { position } = geometry.attributes;
+        console.log(position.count);
+        this.geometry.setAttribute("position", position);
+      }
     }
   }
 }
