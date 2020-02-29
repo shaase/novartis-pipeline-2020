ba56fec56c45f9625577d9519c3391cc40e23498
Stephen Haase <shaase@gmail.com>
2020-02-29 07:39:24 -0700

    some cleanup

--- electron/app/components/Pipeline/components/PipelineRadial/ThreeChart/arc.ts
+++ electron/app/components/Pipeline/components/PipelineRadial/ThreeChart/arc.ts
@@ -1,34 +1,37 @@
 import * as THREE from "three";
 
+const arcGeometry = (
+  startAngle: number,
+  endAngle: number,
+  innerRadius: number,
+  outerRadius: number,
+): THREE.BufferGeometry => {
+  const x1 = Math.cos(endAngle) * innerRadius;
+  const y1 = Math.sin(endAngle) * innerRadius;
+
+  const segment = new THREE.Shape();
+  segment.absarc(0, 0, outerRadius, startAngle, endAngle, false);
+  segment.lineTo(x1, y1);
+  segment.absarc(0, 0, innerRadius, endAngle, startAngle, true);
+
+  const geometry = new THREE.ShapeBufferGeometry(segment, 64);
+  return geometry;
+};
+
 class SegmentArc extends THREE.Mesh {
   constructor(material: THREE.MeshBasicMaterial) {
     const startAngle = 0;
     const endAngle = 1;
     const innerRadius = 150;
     const outerRadius = 200;
-    const x1 = Math.cos(endAngle) * innerRadius;
-    const y1 = Math.sin(endAngle) * innerRadius;
 
-    const segment = new THREE.Shape();
-    segment.absarc(0, 0, outerRadius, startAngle, endAngle, false);
-    segment.lineTo(x1, y1);
-    segment.absarc(0, 0, innerRadius, endAngle, startAngle, true);
-
-    const geometry = new THREE.ShapeBufferGeometry(segment, 64);
+    const geometry = arcGeometry(startAngle, endAngle, innerRadius, outerRadius);
     super(geometry, material);
   }
 
   update(startAngle: number, endAngle: number, innerRadius: number, outerRadius: number): void {
     if (this.geometry instanceof THREE.BufferGeometry) {
-      const x1 = Math.cos(endAngle) * innerRadius;
-      const y1 = Math.sin(endAngle) * innerRadius;
-
-      const segment = new THREE.Shape();
-      segment.absarc(0, 0, outerRadius, startAngle, endAngle, false);
-      segment.lineTo(x1, y1);
-      segment.absarc(0, 0, innerRadius, endAngle, startAngle, true);
-
-      const geometry = new THREE.ShapeBufferGeometry(segment, 64);
+      const geometry = arcGeometry(startAngle, endAngle, innerRadius, outerRadius);
       const { position } = geometry.attributes;
       this.geometry.setAttribute("position", position);
     }
