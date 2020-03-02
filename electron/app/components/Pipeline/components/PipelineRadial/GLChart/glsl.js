e58d0c8441ed0a5c41ab16beaccb0fc6507fa48a
Stephen Haase <shaase@gmail.com>
2020-03-02 10:08:37 -0700

    pulled mesh local

--- electron/app/components/Pipeline/components/PipelineRadial/GLChart/glsl.js
+++ electron/app/components/Pipeline/components/PipelineRadial/GLChart/glsl.js
@@ -1,5 +1,5 @@
 import reglConstructor from "regl";
-import meshConstructor from "glsl-circular-arc";
+import mesh from "./mesh";
 import { frag, vert } from "./shaders";
 
 let regl;
@@ -7,7 +7,6 @@ let update;
 
 export const startGL = canvas => {
   regl = reglConstructor(canvas);
-  const mesh = meshConstructor();
 
   update = regl({
     frag,
@@ -27,40 +26,6 @@ export const startGL = canvas => {
     },
     elements: mesh.cells,
   });
-
-  // regl.frame(context => {
-  //   regl.clear({ color: [0, 0, 0, 1], depth: true });
-  //   const t = context.time;
-  //   const st = Math.sin(t) * 0.5 + 0.5;
-
-  //   menu([
-  //     {
-  //       theta: [0, st * 2 * Math.PI],
-  //       radius: [0.25, 0.5],
-  //       color: [1, 0, 0.5],
-  //     },
-  //     {
-  //       theta: [1 * st * 4, st * 2 * Math.PI],
-  //       radius: [0.5, 0.75],
-  //       color: [0.5, 1, 0],
-  //     },
-  //     {
-  //       theta: [0, (st / 2) * Math.PI + 1],
-  //       radius: [0.75, 1],
-  //       color: [0, 0.5, 1],
-  //     },
-  //     {
-  //       theta: [2, (st / 2) * Math.PI + 3],
-  //       radius: [0.75, 1],
-  //       color: [1, 0.5, 0],
-  //     },
-  //     {
-  //       theta: [4, (st / 2) * Math.PI + 5],
-  //       radius: [0.75, 1],
-  //       color: [1, 1, 0],
-  //     },
-  //   ]);
-  // });
 };
 
 export const updateGL = arcs => {
