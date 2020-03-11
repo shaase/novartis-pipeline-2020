c3ac69ec68f580b48a995c6c4042b8309e0a51a1
Stephen Haase <shaase@gmail.com>
2020-03-11 15:51:29 -0600

    almost have outlines

--- electron/app/components/Pipeline/components/PipelineRadial/RadialChart/shaders.js
+++ electron/app/components/Pipeline/components/PipelineRadial/RadialChart/shaders.js
@@ -31,19 +31,33 @@ export const frag = `
     return (1.0 - d) * step(radius_0.x, r) * step(r, radius_0.y);
   }
 
+  float angleEdge(vec2 pos, vec2 radius_0, vec2 theta_0) {
+    float r = sqrt(pow(pos.x, 2.0) + pow(pos.y, 2.0));
+    float angle = atan(pos.y, pos.x);
+    float diff0 = angle - theta_0.x;
+    float diff1 = theta_0.y - angle;
+    float diff = min(diff0, diff1);
+    return diff0 * r * 3.14;
+  }
+
   varying vec2 vpos;
   uniform float alpha, size;
   uniform vec2 radius, theta;
   uniform vec3 color;
   void main () {
+    float dist = 0.02;
     float re = radialEdge(size, vpos, radius);
+    float ae = angleEdge(vpos, radius, theta);
     vec3 white = vec3(1.0, 1.0, 1.0);
     if (re < 0.01) discard;
-    else if (re < 0.02) gl_FragColor = vec4(mix(color, white, 1.0), alpha);
-    else if (re < 0.04) gl_FragColor = vec4(mix(color, white, 0.8), alpha);
-    else if (re < 0.06) gl_FragColor = vec4(mix(color, white, 0.6), alpha);
-    else if (re < 0.08) gl_FragColor = vec4(mix(color, white, 0.4), alpha);
-    else if (re < 0.10) gl_FragColor = vec4(mix(color, white, 0.2), alpha);
+    else if (re < dist * 1.0) gl_FragColor = vec4(mix(color, white, 1.0), alpha);
+    else if (re < dist * 2.0) gl_FragColor = vec4(mix(color, white, 0.8), alpha);
+    else if (re < dist * 3.0) gl_FragColor = vec4(mix(color, white, 0.6), alpha);
+    else if (re < dist * 4.0) gl_FragColor = vec4(mix(color, white, 0.4), alpha);
+    else if (re < dist * 5.0) gl_FragColor = vec4(mix(color, white, 0.2), alpha);
+    else if (ae < 0.003) gl_FragColor = vec4(mix(color, white, 0.8), alpha);
+    else if (ae < 0.004) gl_FragColor = vec4(mix(color, white, 0.4), alpha);
+    else if (ae < 0.005) gl_FragColor = vec4(mix(color, white, 0.2), alpha);
     else gl_FragColor = vec4(color, alpha);
   }
 `;
